dd06a5eb23cc6d6322992ffc5daef0e6db99ae59
sweeetduude <waytolegend1@gmail.com>
2023-02-28 22:39:23 +0100

    added Result return instead of unwraps

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::fs;
-use std::io::{stdin, stdout, Write};
+use std::io::{stdin, stdout, Error, Write};
 use std::path::PathBuf;
 
-fn write_to_file(output_file: &str, name: &str, path: &str) {
+fn write_to_file(output_file: &str, name: &str, path: &str) -> Result<(), Error> {
     let mut file = fs::OpenOptions::new()
         .create(true)
         .append(true)
-        .open(output_file)
-        .unwrap();
+        .open(output_file)?;
 
-    writeln!(file, "{}\t{}", name, path).unwrap();
+    writeln!(file, "{}\t{}", name, path)?;
+    Ok(())
 }
 
 fn list_files(
@@ -18,8 +18,8 @@ fn list_files(
     write_files: bool,
     write_folders: bool,
     list_all: bool,
-) {
-    let entries = fs::read_dir(path).unwrap();
+) -> Result<(), Error> {
+    let entries = fs::read_dir(path)?;
 
     for entry in entries {
         if let Ok(entry) = entry {
@@ -28,24 +28,26 @@ fn list_files(
                     output_file,
                     &entry.file_name().to_string_lossy(),
                     &entry.path().display().to_string(),
-                );
+                )?;
             } else if entry.path().is_dir() {
                 if write_folders {
                     write_to_file(
                         output_file,
                         &entry.file_name().to_string_lossy(),
                         &entry.path().display().to_string(),
-                    );
+                    )?;
                 }
                 if list_all {
-                    list_files(&entry.path(), output_file, write_files, write_folders, true);
+                    list_files(&entry.path(), output_file, write_files, write_folders, true)?;
                 }
             }
         }
     }
+
+    Ok(())
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
     println!("Choose an option:");
     println!("1. List all folders in the current directory");
     println!("2. List all folders in all underlying directories");
@@ -54,21 +56,22 @@ fn main() {
     println!("5. List both folders and files in the current directory");
     println!("6. List folders and files in all underlying directories");
     print!("Enter your choice: ");
-    stdout().flush().unwrap();
+    stdout().flush()?;
 
-    let mut user_input = String::new();
-    stdin().read_line(&mut user_input).unwrap();
+    let mut choise = String::new();
+    stdin().read_line(&mut choise)?;
+    let user_input = choise.trim().parse::<u32>().unwrap_or(0);
 
-    let write_files = match user_input.trim().to_lowercase().as_str() {
-        "3" | "4" | "5" | "6" => true,
+    let write_files = match user_input {
+        3 | 4 | 5 | 6 => true,
         _ => false,
     };
-    let write_folders = match user_input.trim().to_lowercase().as_str() {
-        "1" | "2" | "5" | "6" => true,
+    let write_folders = match user_input {
+        1 | 2 | 5 | 6 => true,
         _ => false,
     };
-    let recursive = match user_input.trim().to_lowercase().as_str() {
-        "2" | "4" | "6" => true,
+    let recursive = match user_input {
+        2 | 4 | 6 => true,
         _ => false,
     };
 
@@ -79,6 +82,8 @@ fn main() {
             write_files,
             write_folders,
             recursive,
-        );
+        )?;
     }
+
+    Ok(())
 }
